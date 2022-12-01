3c5714777b5bb41677688e25b57fd095deff0901
SimonYu125 <yu.sim@northeastern.edu>
2022-11-30 19:56:12 -0500

    Directions

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print('hello git!')
+# add in GUI calendar application
+# being able to select a certain date
+# a pop out on the date so that you can add events to it
+# being able to save and load the same calendar each time the program runs
