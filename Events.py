5a95e97a0e0d33e725e507c6b5c3a21dfedd56ab
SimonYu125 <yu.sim@northeastern.edu>
2022-12-04 01:15:00 -0500

    Updates

---
+++ Events.py
@@ -0,0 +1,23 @@
+
+from tkcalendar import Calendar
+import tkinter as tk
+from tkinter import ttk
+
+# ask user what type of even they want to add and when. 
+def Events():
+
+    top = tk.Toplevel(root)
+
+    cal = Calendar(top, selectmode='none')
+    date = cal.datetime.today()
+    cal.calevent_create(date+ cal.timedelta(days=2), 'Meeting', 'reminder')
+    cal.calevent_create(date + cal.timedelta(days=3), 'Homework Due', 'homework')
+
+    cal.tag_config('reminder', background='red', foreground='yellow')
+
+    cal.pack(fill="both", expand=True)
+    ttk.Label(top, text="Light Blue = Today\n Red = Homework \n Blue = Meeting.").pack()
+
+root = tk.Tk()
+ttk.Button(root, text='Calendar with events', command=Events).pack(padx=10, pady=10)
+root.mainloop()
--- main.py
+++ main.py
@@ -2,24 +2,5 @@
 # being able to select a certain date
 # a pop out on the date so that you can add events to it
 # being able to save and load the same calendar each time the program runs
-from tkcalendar import Calendar
-import tkinter as tk
-from tkinter import ttk
 
-def Events():
-
-    top = tk.Toplevel(root)
-
-    cal = Calendar(top, selectmode='none')
-    date = cal.datetime.today() + cal.timedelta(days=2)
-    cal.calevent_create(date, 'Meeting', 'reminder')
-    cal.calevent_create(date + cal.timedelta(days=3), 'Homework Due', 'homework')
-
-    cal.tag_config('reminder', background='red', foreground='yellow')
-
-    cal.pack(fill="both", expand=True)
-    ttk.Label(top, text="Light Blue = Today\n Red = Homework \n Blue = Meeting").pack()
-
-root = tk.Tk()
-ttk.Button(root, text='Calendar with events', command=Events).pack(padx=10, pady=10)
-root.mainloop()
+# Main should show the button that user would access to see what they want to do with the calendar
